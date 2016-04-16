eecc293d55f03eadc9a253eb897dbdc678f8ce99
ml4963 <ml4963@nyu.edu>
2016-04-16 17:01:11 -0400

    STUPID MISTAKE

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,36 @@ import os
 from os import listdir
 from os.path import isfile, join
 import foursquare
+import yweather
 
 app = Flask(__name__) 
+
+client = yweather.Client()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('welcome.html')
 
-@app.route('/tags', methods=['GET'])
-def tags():
+@app.route('/schedule', methods=['GET'])
+def schedule():
     
-    client = foursquare.Foursquare(client_id='JEK02X44TGMNJSE0VC1UBEB4FRNNW3UMFQ4IQOPI4BAR2GXA', \
-                                    client_secret='A2Z50VTUHHXEUYJBHCQKB1LXTNVVBYBQR4SDASVTXTWUMWXS') #foursquare shit
+  #  client = foursquare.Foursquare(client_id='JEK02X44TGMNJSE0VC1UBEB4FRNNW3UMFQ4IQOPI4BAR2GXA', \
+   #                                 client_secret='A2Z50VTUHHXEUYJBHCQKB1LXTNVVBYBQR4SDASVTXTWUMWXS') #foursquare shit
 
-    zipcode = request.args["zipcode"]
-    
+    city_state = request.args["city_state"] # New York City, New York
 
-    return render_template('schedule.html')
+    # weather part
+    ID = client.fetch_woeid(city_state)
+    Info = client.fetch_weather(ID)
+
+    # info gotta pass
+    location = (Info["location"]["city"],Info["location"]["region"])
+    temperature = (Info["condition"]["temp"])
+    condition = (Info["condition"]["text"])
+
+    return render_template('schedule.html', location = location, \
+                           temperature = temperature, \
+                           condition = condition)
 
 
 if __name__ == "__main__":
